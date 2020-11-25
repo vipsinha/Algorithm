fa6a0eb06ba0742872df0d0fbfa72d8673eeefe1
Vipul Sinha <vipulsinha@Vipuls-Air.fritz.box>
2020-11-25 09:30:02 +0100

    Added the hash algo code

--- Sorting/6_HashAlgorithm.c
+++ Sorting/6_HashAlgorithm.c
@@ -2,50 +2,58 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define TABLE_LEN 100
 #define STRING_LEN 10
+#define KEY_LEN 5
 
 struct ITEM{
-    int key;
+    char* key;
     char* value;
 };
 
 typedef struct ITEM items;
 
-items* createATable(int size);
-items* insertData(items* node);
-void printData(items* node, int size);
-void searchData(items* node, int size);
-int hashTable(int key);
+struct TABLE{
+    items** itemsOnTable;
+    int size;
+    int count;
+};
+
+typedef struct TABLE table;
+
+table* createTable(void);
+items* createItem(char* key, char* value);
+table* insertData(table* node);
+void printData(table* node);
+void searchData(table* node);
+int hashTable(char* key, int size);
 
 int main(){
 
     int choice =0;
     int size = 0;
-    items* node;
+    table* tableNode;
 
     do{
         printf("Enter 1 to exit\n");
-        printf("Enter 2 to create a value in hash\n");
-        printf("Enter 3 to Enter the key and value\n");
+        printf("Enter 2 to create a table\n");
+        printf("Enter 3 to enter key and value\n");
         printf("Enter 4 to search key value pair\n");
-        printf("Enter 5 to show values in hash\n"); 
+        printf("Enter 5 to show table\n"); 
         printf("Enter choice\n");
         scanf("%d", &choice);
 
         if(choice == 2){
-
-            printf("Enter the node size\n");
-            scanf("%d",&size);
-            node = createATable(size);
+            tableNode = createTable();
         }  
         else if(choice == 3){
-            node = insertData(node);
+            tableNode = insertData(tableNode);
         }   
         else if(choice ==4){
-            searchData(node, size);
+            searchData(tableNode);
         }
         else if(choice ==5){
-            printData(node, size);
+            printData(tableNode);
         }
         else{
             choice = 1;
@@ -55,62 +63,84 @@ int main(){
 
     return 0;
 }
-int hashTable(int key){
+int hashTable(char* key, int size){
     int hashValue;
 
-    hashValue = (key % 0xA);
-    return (hashValue);
+    for(int i=0; i<strlen(key); i++)
+        hashValue += key[i];
+    return (hashValue % size);
 }
 
-items* createATable(int size){
-    items* node;
+table* createTable(void){
+    table* node = (table*)malloc(sizeof(table));
 
-    node = (items*)malloc(size*sizeof(items));
-    for(int i=0; i<size; i++){
-        node[i].key = 0;
-        node[i].value = (char*)calloc(STRING_LEN, sizeof(char));
+    printf("Enter the node size\n");
+    scanf("%d",&node->size);
+
+    node->itemsOnTable = (items**)calloc(node->size, sizeof(items*));
+    for(int i=0; i<node->size; i++){
+        node->itemsOnTable[i] = NULL;
     }
+
+    node->count = 0;
         
     return (node);
 }
 
-items* insertData(items* node){
-    int key;
-    char* data=(char*)malloc(STRING_LEN*sizeof(char));
-    char temp[20];
+items* createItem(char* key, char* value){
+    items* node = (items*)malloc(sizeof(items));
+    
+    node->key = (char*)malloc(strlen(key)+1);
+    node->value = (char*)malloc(strlen(value)+1);
+
+    strcpy(node->key, key);
+    strcpy(node->value, value);
+        
+    return (node);
+}
+
+table* insertData(table* node){
+    char* key = (char*)malloc(KEY_LEN*sizeof(char));
+    char* data = (char*)malloc(STRING_LEN*sizeof(char));
     int hash;
 
-    printf("Enter the key and value of length 10\n");
-    scanf("%d",&key);
-    fgets(data, STRING_LEN, stdin);
-    hash = hashTable(key);
-    node[hash].key = hash;
-    strcpy(node[hash].value, data);
+    if(node->count!= node->size){
+        printf("Enter the key and value\n");
+        scanf("%s", key);
+        scanf("%s", data);
 
+        hash = hashTable(key, node->size);
+        node->itemsOnTable[hash] = createItem(key, data);
+        node->count++;
+    }
+    else{
+        printf("Table is full\n");
+    }
     return (node);    
 }
 
-void searchData(items* node, int size){
-    int key, hash;
+void searchData(table* node){
+    char* key = (char*)malloc(KEY_LEN*sizeof(char));
+    int hash;
 
     printf("Enter the key to search\n");
-    scanf("%d", &key);
+    scanf("%s", key);
 
-    hash = hashTable(key);
+    hash = hashTable(key, node->size);
 
-    if(hash == node[hash].key){
-        printf("Value = %s\n", node[hash].value);
+    if(strcmp(key, node->itemsOnTable[hash]->key) == 0){
+        printf("Value = %s\n", node->itemsOnTable[hash]->value);
+        printf("Hash = %d\n", hash);
     }
     else{
         printf("Key is not there\n");
     }
-
 }
 
-void printData(items* node, int size){
-    for(int i=0; i<size; i++){
-        if(node[i].key!=0){
-            printf("\n%d - %s ",node[i].key, node[i].value);
-        }
+void printData(table* node){
+    for(int i=0; i<node->size; i++){
+        if(node->itemsOnTable[i]!= NULL){
+            printf("Values = %s\n", node->itemsOnTable[i]->value);
+        }        
     }
 }
