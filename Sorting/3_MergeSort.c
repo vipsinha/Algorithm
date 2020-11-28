a697c3294bdb051737c9f61c5de314e0892a5a20
Vipul Sinha <vipulsinha@Vipuls-Air.fritz.box>
2020-11-28 10:34:40 +0100

    Updated the name

--- Sorting/3_MergeSort.c
+++ Sorting/3_MergeSort.c
@@ -7,7 +7,7 @@ https://en.wikipedia.org/wiki/Insertion_sort
 void EnterAnArray(int* array, int size);
 void PrintAnArray(int* array, int size);
 void MergeSort(int* array,int size);
-void MergeSort_Split(int* TargetArray, int* SourceArray, int Start, int End);
+void MergeSort_Split(int* SrcArray, int* TargetArray, int Start, int End);
 void MergeSort_Join(int* SrcArray, int* TargetArray, int Start, int Middle, int End);
 void CopyArray(int A[], int Start, int End, int B[]);
 
@@ -44,38 +44,38 @@ void PrintAnArray(int* array, int size){
 
 void MergeSort(int* array,int size){
 
-    int *TargetArray = (int*)malloc(size * sizeof(int));
+    int *SrcArray = (int*)malloc(size * sizeof(int));
     int Start = 0;
     int End = size;
 
-    CopyArray(array, 0, size, TargetArray);
-    MergeSort_Split(TargetArray, array, 0, size);
+    CopyArray(array, 0, size, SrcArray);
+    MergeSort_Split(SrcArray, array, 0, size);
 }
 
-void MergeSort_Split(int* TargetArray, int* SourceArray, int Start, int End)
+void MergeSort_Split(int* SrcArray, int* TargetArray, int Start, int End)
 {
     if(End - Start <= 1)                       // if run size == 1
         return;                                 //   consider it sorted
     // split the run longer than 1 item into halves
     int Middle = (End + Start) / 2;              // Middle = mid point
     // recursively sort both runs from array A[] into B[]
-    MergeSort_Split(SourceArray, TargetArray, Start, Middle);  // sort the left  run
-    MergeSort_Split(SourceArray, TargetArray, Middle, End);  // sort the right run
+    MergeSort_Split(TargetArray, SrcArray, Start, Middle);  // sort the left  run
+    MergeSort_Split(TargetArray, SrcArray, Middle, End);  // sort the right run
     // merge the resulting runs from array B[] into A[]
-    MergeSort_Join(TargetArray, SourceArray, Start, Middle, End);
+    MergeSort_Join(SrcArray, TargetArray, Start, Middle, End);
 }
 
-void MergeSort_Join(int* TargetArray, int* SourceArray, int Start, int Middle, int End){
+void MergeSort_Join(int* SrcArray, int* TargetArray, int Start, int Middle, int End){
     int i = Start;
     int j = Middle;
     int k;
 
     for(k=Start; k<End; k++){
-        if((i<Middle) && ((j>=End) || (TargetArray[i]<=TargetArray[j]))){
-            SourceArray[k] = TargetArray[i];
+        if((i<Middle) && ((j>=End) || (SrcArray[i]<=SrcArray[j]))){
+            TargetArray[k] = SrcArray[i];
             i++;
         }else{
-            SourceArray[k] = TargetArray[j];
+            TargetArray[k] = SrcArray[j];
             j++;
         }
     }
